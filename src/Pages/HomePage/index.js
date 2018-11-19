e44b19e42250845463475fb0ba8a32d56b450727
Subham Sengupta <sengupta335@gmail.com>
2018-11-19 23:19:17 +0530

    Adds PageAppBar Component

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BigBanner from '../../Components/BigBanner'
+import PageAppBar from '../../Components/PageAppBar'
 
 import HomePageBanner from './assets/home_page_banner.jpg'
 
 const HomePage = () => (
   <div className='home-page'>
-    <BigBanner src={HomePageBanner} alt='home_page_banner' />
+    <BigBanner src={HomePageBanner} />
+    <PageAppBar />
   </div>
 )
 

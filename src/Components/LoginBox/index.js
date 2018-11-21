b810bbee6f598dee5ea6b67b50f669189d3b0150
Souvik Banerjee <banerjeesouvik18@hotmail.co.uk>
2018-11-22 00:21:07 +0530

    add roboto font and add password and login button in login card

--- src/Components/LoginBox/index.js
+++ src/Components/LoginBox/index.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 import './style.css'
 
@@ -23,7 +24,7 @@ class LoginBox extends React.Component {
   }
 
   render() {
-    const { email } = this.state
+    const { email, password } = this.state
     return (
       <div className='page-login-box'>
         <Card className='page-login-box-card'>
@@ -31,17 +32,37 @@ class LoginBox extends React.Component {
             <Typography className='page-login-box-card-title' variant='display1' gutterBottom>
               Get started with us.
             </Typography>
-            <TextField
-              id='page-login-box-email'
-              label='Your Email ID'
-              className='page-login-box-card-text-field'
-              name='email'
-              value={email || ''}
-              onChange={this.handleChange}
-              margin='normal'
-              variant='outlined'
-              fullWidth
-            />
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                id='page-login-box-email'
+                label='Your Email ID'
+                className='page-login-box-card-text-field'
+                name='email'
+                value={email || ''}
+                type='email'
+                onChange={this.handleChange}
+                margin='normal'
+                variant='outlined'
+                fullWidth
+              />
+              <TextField
+                id='page-login-box-password'
+                label='Your Password'
+                className='page-login-box-card-text-field'
+                name='password'
+                value={password || ''}
+                type='password'
+                onChange={this.handleChange}
+                margin='normal'
+                variant='outlined'
+                fullWidth
+              />
+              <div className='page-login-box-action-btn'>
+                <Button variant='contained' color='primary' size='large'>
+                  LogIn
+                </Button>
+              </div>
+            </form>
           </CardContent>
         </Card>
       </div>

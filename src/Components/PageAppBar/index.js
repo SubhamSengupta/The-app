a43871aeaefba01a501c084e55956a9dcb3b69d6
Subham Sengupta <sengupta335@gmail.com>
2019-01-07 22:11:15 +0530

    Removes mui code

--- src/Components/PageAppBar/index.js
+++ src/Components/PageAppBar/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
 
 import './style.css'
 
 const PageAppBar = () => (
-  <div className='page-app-bar'>
-    <AppBar classes={{ root: 'page-app-bar-root' }}>
-      <div className='header-logo' />
-      <ul>
-        <li>About Us</li>
-      </ul>
-    </AppBar>
-  </div>
+  <div className='page-app-bar' />
 )
 
 export default PageAppBar

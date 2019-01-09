cf155c542020b6b40a0c7603703b1a2a66fdf595
Subham Sengupta <sengupta335@gmail.com>
2019-01-09 22:46:11 +0530

    Adds title in appBar

--- src/Components/PageAppBar/index.js
+++ src/Components/PageAppBar/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 import './style.css'
 
 const PageAppBar = () => (
-  <div className='page-app-bar' />
+  <div className='page-app-bar'>
+    <div className='page-app-bar__title'>The App</div>
+  </div>
 )
 
 export default PageAppBar
